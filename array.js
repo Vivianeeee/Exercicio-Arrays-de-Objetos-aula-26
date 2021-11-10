e12523f28b477f2a0a3ae30d48ed46812a87fb79
vivianeeee <vivianeoliveirandrade@gmail.com>
2021-11-10 08:19:31 -0300

    Valores Promoção Aula 26

--- array.js
+++ array.js
@@ -1,60 +1,4 @@
 // Array de objetos 
-/// EXERCÍCIO 1
-const  alunas  =  [
-    {
-        id : 1 ,
-        primeiroNome : 'Amanda' ,
-        lastName : 'Mendes' ,
-        idade : '16' ,
-        materiaDestaque : 'Espanhol' ,
-        notas : [ 10 ,  5 ,  8 ,  9,5 ,  7 ,  8 ] ,
-        endereço : [ 'Av. Atlântica ' ,  1.360 ,  ' SP ' ] ,
-    } ,
-    {
-        id : 2 ,
-        primeiroNome : 'Liz' ,
-        lastName : 'Alves' ,
-        idade : 19 ,
-        materiaDestaque : 'Filosofia' ,
-        notas : [ 8 ,  9,5 ,  10 ,  9 ,  7 ,  5 ] ,
-        endereço : [ 'Rua dos Caranguejos' ,  1.608 ,  'SP' ] ,
-
-    } ,
-    {
-        id : 3 ,
-        primeiroNome : 'Luana' ,
-        lastName : 'Silva' ,
-        idade : '18' ,
-        materiaDestaque : 'Inglês' ,
-        notas : [ 10 ,  8 ,  6 ,  9 ,  5 , 10 ] ,
-        endereço : [ 'Rua Antônio Carlos Gomes' ,  190 , 'SP' ] ,
-    } ,
-] ;
-
-
-para ( deixar  aluna  =  0 ;  aluna  <  Alunas . comprimento ;  aluna ++ ) {  // Aqui IRA percorrer como Alunas e SEUS matriz
-    let  nota  =  Alunas [ aluna ] . notas ;
-    let  total  =  0 ;
-    for ( deixe  i  =  0 ;  i  <  nota . comprimento ;  i ++  ) {  // irá percorrer os array dos arrays
-        total  =  total  +  nota [ i ] ;  // Para somar as notas
-        media  =  total  /  nota [ i ]  // para dividir como notas pela contidade de notas
-        function  alunaMedia ( mediaTotal  =  media ) {
-            if  ( mediaTotal  >  7 ) {
-                voltar  'Aprovada'
-            } else {
-                retornar  'Reprovada'
-            }
-        }
-
-    }   
-
-    console . log ( `Aluna $ { alunas [ aluna ] . firstName } com como notas ( $ { alunas [ aluna ] . notas } ), mora no endereco: $ { alunas [ Aluna ] . endereco } . Teve uma mídia $ { mídia } , portanto foi $ { alunaMedia ( ) } ` ) ;
-}
-
-console . log ( '' )
-
-
-
 
 // /// EXERCÍCIO 2   
 const  pizza  =  [
@@ -95,4 +39,24 @@ function  promocaoDoDia ( ) {
 
 }
 console . log ( `Uma pizza $ { pizza [ cont ] . sabor . toUpperCase ( ) } ou $ { pizza [ cont ] . sabor . substr ( 1 , 5 ) } , $ { promocaoDoDia ( ) } ` )
+}
+
+para ( deixe  cont  =  0 ;  cont  <  pizza . comprimento ;  cont ++ ) {
+    deixe  promoDia  =  pizza [ cont ] . promoção
+    deixe  valor  =  50 ;
+
+    function  promocaoDoDia ( ) {
+        if ( promoDia  ==  true ) {
+            return   'está com desconto de 25%' ;
+            deixe  desc  =  ( valor  *  0,25 ) ;
+            deixe  totalDesc  =  valor  -  desc ;
+            return  `está por apenas $ $ { totalDesc } reais, promoção do dia 25% de desconto. Proveite`
+
+        } else {
+            return  'não está na promoção' ;
+        }
+
+    }
+    console . log ( `Uma pizza $ { pizza [ cont ] . sabor . toUpperCase ( ) } ou $ { pizza [ cont ] . sabor . substr ( 1 , 5 ) } , $ { promocaoDoDia ( ) } ` )
+    console . log ( `Uma pizza $ { pizza [ cont ] . sabor . toUpperCase ( ) } ou $ { pizza [ cont ] . sabor . substr ( 1 , 5 ) } de $ $ { valor } reais , $ { promocaoDoDia ( ) } ! ` )
 }
